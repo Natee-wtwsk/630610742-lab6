e103050d1f0ec973dc9050302568de69b622eb9f
Natee <gg977120@gmail.com>
2021-11-26 16:16:46 +0700

    YES!

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,15 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    int input,even = 0,odd = 0;
+    while(true){
+        cout << "Enter an integer: ";
+        cin >> input;
+        if(input == 0) break;
+        if(input%2 == 0) even++;
+        if(input%2 == 1) odd++;
+    }
+    cout << "#Even numbers = " << even << endl;
+    cout << "#Odd numbers = " << odd;
     return 0;
 }
